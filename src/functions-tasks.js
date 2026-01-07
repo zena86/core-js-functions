ccda964b34a193e180e2010fa615d1b16dc75593
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-01-07 12:04:28 +0400

    feat: solve logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -186,8 +186,19 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function innerFunc(...args) {
+    const argsString = args.join(', ');
+    const funcName = func.name;
+
+    logFunc(`${funcName}(${argsString}) starts`);
+
+    const result = func(...args);
+
+    logFunc(`${funcName}(${argsString}) ends`);
+
+    return result;
+  };
 }
 
 /**
