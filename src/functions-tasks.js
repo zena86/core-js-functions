dd56623d485cea791420427af823886651ada25c
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-01-07 13:03:40 +0400

    feat: solve getFunctionBody

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,12 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (typeof func !== 'function') {
+    return '';
+  }
+
+  return func.toString();
 }
 
 /**
